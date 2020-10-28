86862cba526db115e6e26382a9db7ff9d8e740c1
Victor <a320663@uach.mx>
2020-10-28 00:18:14 -0700

    projects crud

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,22 +1,98 @@
-function list(req, res) {
-    res.send('respond with a project');
+const async = require('async');
+const bcrypt = require('bcrypt');
+const { json } = require('express');
+const Projects = require('../models/project');
+
+function list(req, res, next) {
+  const page = req.params.page ? req.params.page : 1;
+  Projects.paginate({}, {page: page, limit: 100}).then(project => res.status(200).json({
+      message: 'Proyectos encontrados correctamente',
+      objs: project
+  })).catch(error => res.status(500).json({
+      message: 'No se pudieron encontrar los proyectos',
+      obj: error
+  }));
   }
   
   function index(req, res) {
-    var id = req.params.id;
-    res.send('findById: ' + id);
+    let id = req.params.id;
+    Projects.findOne({_id: id}).then(user => res.status(200).json({
+        message: 'Proyecto encontrado correctamente',
+        objs: project
+    })).catch(error => res.status(500).json({
+        message: 'No se pudo encontrar el proyecto',
+        obj: error
+    }));
   }
   
   function create(req, res) {
-    res.send('un post');
+
+    let proyectName = req.body.projectName;
+    let requestDate = req.body.requestDate;
+    let startDate = req.body.startDate;
+    let proyectDescription = req.body.proyectDescription;
+    let managerId = req.body.managerId;
+    let ownerId = req.body.ownerId;
+    let teamLds = req.body.teamLds;
+
+    let project = new Projects({
+      _proyectName : proyectName,
+      _requestDate : requestDate,
+      _startDate : startDate,
+      _proyectDescription : proyectDescription,
+      _managerId : managerId,
+      _ownerId : ownerId,
+      _teamLds : teamLds
+    });
+
+    project.save().then(obj => res.status(200).json({
+      message : 'proyecto creado correctamente',
+      objs : project
+    })).catch(err=> res.status(500).json({
+      message : 'no se pudo almacenar el proyecto',
+      objs : err
+    }));
+
   }
   
   function update(req, res) {
-    res.send('un update');
+    let id = req.params.id;
+
+    let project = new Object();
+
+    if(req.body.projectName)
+        user._proyectName = req.body.projectName;
+    if(req.body.requestDate)
+        user._requestDate = req.body.requestDate;
+    if(req.body.startDate)
+        user._startDate = req.body.startDate;
+    if(req.body.proyectDescription)
+        user._proyectDescription = req.body.proyectDescription;
+    if(req.body.managerId)
+        user._managerId = req.body.managerId;
+    if(req.body.ownerId)
+        user._ownerId = req.body.ownerId;
+    if(req.body.teamLds)
+        user._teamLds = req.body.teamLds;
+
+    Projects.findOneAndUpdate({_id: id}, project, {omitUndefined: true}).then(project => res.status(200).json({
+        message: 'Proyecto actualizado correctamente',
+        objs: project
+    })).catch(error => res.status(500).json({
+        message: 'No se pudo actualizar el proyecto',
+        obj: error
+    }));
   }
   
   function destroy(req, res) {
-    res.send('un delete');
+    const id = req.params.id;
+    Projects.deleteOne({_id: id}).then(project => res.status(200).json({
+        message: 'Proyecto eliminado correctamente',
+        objs: project
+    })).catch(error => res.status(500).json({
+        message: 'No se pudo eliminado el proyecto',
+        obj: error
+    }));
   }
   
   module.exports = {
---
+++ models/project.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+const projects = require('../controllers/projects');
+const user = require('./user');
+
+const schema = mongoose.Schema({
+    _proyectName: String,
+    _requestDate : Date,
+    _startDate : Date,
+    _proyectDescription : String,
+    _managerId : mongoose.Types.ObjectId,
+    _ownerId : mongoose.Types.ObjectId,
+    _teamLds : [mongoose.Types.ObjectId]
+});
+
+class Project{
+
+    constructor(proyectName, requestDate, startDate, proyectDescription, managerId, ownerId, teamLds){
+        this._proyectName = proyectName;
+        this._requestDate = requestDate;
+        this._startDate = startDate;
+        this._proyectDescription = proyectDescription;
+        this._ownerId = ownerId;
+        this._managerId = managerId;
+        this._teamLds = teamLds;
+    }
+    get proyectName() {
+        return this._proyectName;
+    }
+
+    set proyectName(v) {
+        this._proyectName = v;
+    }
+
+    get requestDate() {
+        return this.__requestDate;
+    }
+
+    set requestDate(v) {
+        this.__requestDate = v;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(v) {
+        this._startDate = v;
+    }
+
+    get proyectDescription() {
+        return this._proyectDescription;
+    }
+
+    set proyectDescription(v) {
+        this._proyectDescription = v;
+    }
+    
+    get ownerId() {
+        return this._ownerId;
+    }
+
+    set ownerId(v) {
+        this._ownerId = v;
+    }
+
+    get managerId() {
+        return this._managerId;
+    }
+
+    set managerId(v) {
+        this._managerId = v;
+    }
+
+    get teamLds() {
+        return this._teamLds;
+    }
+
+    set teamLds(v) {
+        this._teamLds = v;
+    }
+}
+
+
+schema.plugin(mongoosePaginate);
+schema.loadClass(Project);
+module.exports = mongoose.model('Project', schema);
